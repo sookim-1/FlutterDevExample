5aa86310b29b83f00841a039c45e9697ad61cde8
sookim-1 <scstnghks@gmail.com>
2025-03-17 18:45:39 +0900

    feat : 카카오로그인 - iOS 구현 with dotenv

--- sns_login_flutter/README.md
+++ sns_login_flutter/README.md
@@ -2,15 +2,78 @@
 
 SNS로그인 Flutter 예제
 
-## Getting Started
+# dotenv APIKey 숨김처리
+공식패키지 : [flutter_dotenv](https://pub.dev/packages/flutter_dotenv)
 
-This project is a starting point for a Flutter application.
+1. 의존성 설치
+   pubspec.yaml
 
-A few resources to get you started if this is your first Flutter project:
+```yaml
+dependencies:
+  flutter_dotenv: ^5.2.1
+```
+2. 설정
+- `.env` 파일 추가
+   ```
+  KAKAO_NATIVE_APP_KEY=123456789ab
+  ```
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- 추가한 파일 pubspec.yaml assets에 .env 경로 추가
+   ```yaml
+   assets:
+      - assets/config/.env
+   ```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- `.gitignore`에 *.env 추가
+
+3. 로드
+
+```dart
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future main() async {
+   await dotenv.load(fileName: "assets/config/.env");
+   String api_key = dotenv.env['KAKAO_NATIVE_APP_KEY'];
+   //...runapp
+}
+```
+## iOS
+- `API_KEY.xcconfig` 파일 추가
+   ```
+  KAKAO_NATIVE_APP_KEY=123456789ab
+  ```
+- `.gitignore`에 *.API_KEY.xcconfig 추가
+- Debug.xcconfig, Release.xcconfig에 API_KEY.xcoonfig Include 
+  주의할점은 Generated.xcconfig보다 먼저 include 해줘야합니다.
+   ```
+  #include? "Pods/Target Support Files/Pods-Runner/Pods-Runner.debug.xcconfig"
+   #include "API_KEY.xcconfig"
+   #include "Generated.xcconfig"
+  ```
+
+
+
+
+# 카카오 로그인 
+
+공식문서 : [Kakao Developers](https://developers.kakao.com/docs/latest/ko/kakaologin/flutter#set-custom-url-scheme)
+pub.dev : [developers.kakao.com](https://pub.dev/publishers/developers.kakao.com/packages)
+
+1. 의존성 설치
+pubspec.yaml
+
+```yaml
+dependencies:
+  kakao_flutter_sdk_user: ^1.9.5 # 카카오 로그인 API 패키지
+```
+
+2. 카카오 내 애플리케이션 생성
+
+- iOS 번들 ID 등록
+- 안드로이드 패키지명 등록
+
+
+## iOS
+Info.plist - Queried URL Schemes 배열 - Item에 `kakaokompassauth` 값 추가
+URL Types - URL Scemes `kakao${NativeAppKey}` 추가
+
--- sns_login_flutter/lib/main.dart
+++ sns_login_flutter/lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "assets/config/.env");
+  WidgetsFlutterBinding.ensureInitialized();
+  String? kakaoNativeAppKey = dotenv.env['KAKAO_NATIVE_APP_KEY'];
+
+  KakaoSdk.init(
+    nativeAppKey: kakaoNativeAppKey,
+  );
+
   runApp(const MyApp());
 }
 
@@ -45,13 +57,16 @@ class _MyHomePageState extends State<MyHomePage> {
           spacing: 20.0,
           children: <Widget>[
             ElevatedButton(onPressed: _touchedKakao, child: Text('카카오 로그인')),
-            ElevatedButton(onPressed: _touchedFacebook, child: Text('페이스북 로그인')),
+            ElevatedButton(
+                onPressed: _touchedFacebook, child: Text('페이스북 로그인')),
             ElevatedButton(onPressed: _touchedGoogle, child: Text('구글 로그인')),
             ElevatedButton(onPressed: _touchedApple, child: Text('애플 로그인')),
-
-            Text(
-              '$_snsData',
-              style: Theme.of(context).textTheme.bodyLarge,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                '$_snsData',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
             ),
           ],
         ),
@@ -59,10 +74,70 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _touchedKakao() {
-    setState(() {
-      _snsData = '카카오 로그인';
-    });
+  void _touchedKakao() async {
+    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        await UserApi.instance.loginWithKakaoTalk();
+        User user = await UserApi.instance.me();
+
+        setState(() {
+          _snsData = '카카오톡 로그인 성공'
+              '\n사용자 정보 요청 성공'
+              '\n회원번호: ${user.id}'
+              '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+              '\n프로필 URL: ${user.kakaoAccount?.profile?.profileImageUrl}';
+        });
+      } catch (error) {
+        setState(() {
+          _snsData = '카카오톡 로그인 실패'
+              '\n ${error}';
+        });
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          await UserApi.instance.loginWithKakaoAccount();
+          User user = await UserApi.instance.me();
+
+          setState(() {
+            _snsData = '카카오톡 연결된 계정이 없는 경우 로그인 성공'
+                '\n사용자 정보 요청 성공'
+                '\n회원번호: ${user.id}'
+                '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+                '\n이메일: ${user.kakaoAccount?.email}';
+          });
+        } catch (error) {
+          setState(() {
+            _snsData = '카카오톡 연결된 계정이 없는 경우 로그인 실패'
+                '\n ${error}';
+          });
+        }
+      }
+    } else {
+      try {
+        await UserApi.instance.loginWithKakaoAccount();
+        User user = await UserApi.instance.me();
+
+        setState(() {
+          _snsData = '카카오계정 로그인 성공'
+              '\n사용자 정보 요청 성공'
+              '\n회원번호: ${user.id}'
+              '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+              '\n이메일: ${user.kakaoAccount?.email}';
+        });
+      } catch (error) {
+        setState(() {
+          _snsData = '카카오계정 로그인 실패'
+              '\n ${error}';
+        });
+      }
+    }
   }
 
   void _touchedFacebook() {
@@ -82,5 +157,4 @@ class _MyHomePageState extends State<MyHomePage> {
       _snsData = '애플 로그인';
     });
   }
-
 }
